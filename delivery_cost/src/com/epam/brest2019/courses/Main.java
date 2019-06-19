c9aaf1c9a319565730dbc1eb68a16fabf8dc2d2a
RomanukAlex <Romanuk_Alex@tut.by>
2019-06-19 19:33:30 +0300

    commit test03

--- delivery_cost/src/com/epam/brest2019/courses/Main.java
+++ delivery_cost/src/com/epam/brest2019/courses/Main.java
@@ -5,15 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hi!!");
         System.out.println("Hi22");
-//TODO: next
-        System.out.println("Prum");
-        Integer newName = 12;
-        Integer a = 12;
-        System.out.println(newName);
-        System.out.println(a);
-        System.out.println("aaaaa");
-        System.out.println(a);
-
     }
 
 } 
