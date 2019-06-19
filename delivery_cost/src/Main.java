5d031001dec280dba6da7be8c1fc0285acc582b4
RomanukAlex <Romanuk_Alex@tut.by>
2019-06-19 18:46:21 +0300

    commit

--- delivery_cost/src/Main.java
+++ delivery_cost/src/Main.java
@@ -1,7 +1,8 @@
-public class Main{
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hi!!");
         System.out.println("Hi22");
+        System.out.println("Prum");
     }
 
 } 
