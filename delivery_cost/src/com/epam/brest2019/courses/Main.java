ede6d45fb753161badcd734bf3146325e7600747
RomanukAlex <Romanuk_Alex@tut.by>
2019-06-19 19:22:47 +0300

    commit test

--- delivery_cost/src/com/epam/brest2019/courses/Main.java
+++ delivery_cost/src/com/epam/brest2019/courses/Main.java
@@ -1,8 +1,15 @@
+package com.epam.brest2019.courses;
+
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hi!!");
         System.out.println("Hi22");
+//TODO: next
         System.out.println("Prum");
+        Integer newName = 12;
+        Integer a = 12;
+        System.out.println(newName);
     }
 
 } 
